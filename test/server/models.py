ee525e01d5b22b218a34a0b60a3b3f54ef785fae
Mark <alexco1234@gmail.com>
2026-01-21 21:16:14 -0600

    deleted testing directory from repo

--- test/server/models.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy_serializer import SerializerMixin
-from config import db
-
-class User(db.Model, SerializerMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
--- test/server/seed.py
+++
@@ -1,8 +0,0 @@
-from config import app, db
-from models import User
-
-with app.app_context():
-    User.query.delete()
-    user = User(name="Example User")
-    db.session.add(user)
-    db.session.commit()
